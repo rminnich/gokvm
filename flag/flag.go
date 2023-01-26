9d7af0131084f6fe6c7d5ea013d05e54a96bbbb7
Ronald G. Minnich <rminnich@google.com>
2023-01-26 13:19:58 +0000

    DRAFT: Add support for routes from local sockets to vsock
    This is purely the first step, allowing users to
    define places where gokvmk will do a Listen() and
    connect to the vm.
    
    Signed-off-by: Ronald G. Minnich <rminnich@google.com>

--- flag/flag.go
+++ flag/flag.go
@@ -3,9 +3,37 @@ package flag
 import (
 	"flag"
 	"fmt"
+	"math/rand"
 	"strconv"
 	"strings"
+	"time"
 )
+
+// A VSockRoute is a string
+// describing a VM address
+// and a local address.
+type VSockRoute struct {
+	VMPort    uint32
+	Net, Addr string
+}
+
+type VSockRoutes []*VSockRoute
+
+// Stringer for VSockRoute
+func (v *VSockRoute) String() string {
+	return fmt.Sprintf("%d=%q:%q", v.VMPort, v.Net, v.Addr)
+}
+
+func (v VSockRoutes) String() string {
+	var s string
+	for _, r := range v {
+		if len(s) > 0 {
+			s = s + ","
+			s = s + r.String()
+		}
+	}
+	return s
+}
 
 // Config defines the configuration of the
 // virtual machine, as determined by flags.
@@ -19,6 +47,9 @@ type Config struct {
 	NCPUs      int
 	MemSize    int
 	TraceCount int
+	Routes     VSockRoutes
+	Vsock      string
+	CID        uint32
 }
 
 // ParseSize parses a size string as number[gGmMkK]. The multiplier is optional,
@@ -53,9 +84,40 @@ func ParseSize(s, unit string) (int, error) {
 	return -1, fmt.Errorf("can not parse %q as num[gGmMkK]:%w", s, strconv.ErrSyntax)
 }
 
+func ParseRoutes(route ...string) (VSockRoutes, error) {
+	var routes VSockRoutes
+
+	for _, r := range route {
+		a := strings.Split(r, "=")
+		if len(a) != 2 {
+			return nil, fmt.Errorf("%q not in form vsockport=net:port:%w", r, strconv.ErrSyntax)
+		}
+
+		local := strings.Split(a[1], ":")
+		if len(local) != 2 {
+			return nil, fmt.Errorf("%q not in form vsockport=net:port:%w", r, strconv.ErrSyntax)
+		}
+
+		port, err := strconv.ParseUint(a[0], 0, 32)
+		if err != nil {
+			return nil, fmt.Errorf("%q:%w", a[0], err)
+		}
+
+		routes = append(routes, &VSockRoute{VMPort: uint32(port), Net: local[0], Addr: local[1]})
+	}
+
+	return routes, nil
+}
+
 // ParseArgs calls flag.Parse and a *Config or error.
 func ParseArgs(args []string) (*Config, error) {
 	c := &Config{}
+
+	// There is almost no case where letting users pick the CID
+	// ends well, so it will not be an option.
+	rand.Seed(time.Now().UnixNano())
+	c.CID = uint32(rand.Intn(0xffffffff-3) + 3)
+
 	flag.StringVar(&c.Dev, "D", "/dev/kvm", "path of kvm device")
 	flag.StringVar(&c.Kernel, "k", "./bzImage", "kernel image path")
 	flag.StringVar(&c.Initrd, "i", "./initrd", "initrd path")
@@ -66,11 +128,13 @@ func ParseArgs(args []string) (*Config, error) {
 		`virtio_pci.force_legacy=1 rdinit=/init init=/init`, "kernel command-line parameters")
 	flag.StringVar(&c.TapIfName, "t", "tap", "name of tap interface")
 	flag.StringVar(&c.Disk, "d", "/dev/zero", "path of disk file (for /dev/vda)")
+	flag.StringVar(&c.Vsock, "vsock", "/dev/vhost-vsock", "path of disk vsock device")
 
 	flag.IntVar(&c.NCPUs, "c", 1, "number of cpus")
 
 	msize := flag.String("m", "1G", "memory size: as number[gGmM], optional units, defaults to G")
 	tc := flag.String("T", "0", "how many instructions to skip between trace prints -- 0 means tracing disabled")
+	routes := flag.String("R", "", "1 or more vsock routes in the form vsockport=type:port[...], e.g. 17010=unix:file or 17010=tcp:17010")
 
 	flag.Parse()
 
@@ -84,6 +148,14 @@ func ParseArgs(args []string) (*Config, error) {
 	}
 
 	if c.TraceCount, err = ParseSize(*tc, ""); err != nil {
+		return nil, err
+	}
+
+	if len(*routes) == 0 {
+		return c, nil
+	}
+
+	if c.Routes, err = ParseRoutes(strings.Split(*routes, ",")...); err != nil {
 		return nil, err
 	}
 
--- flag/flag_test.go
+++ flag/flag_test.go
@@ -2,11 +2,49 @@ package flag_test
 
 import (
 	"errors"
+	"reflect"
 	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/bobuhiro11/gokvm/flag"
 )
+
+func TestParseRoutes(t *testing.T) {
+	t.Parallel()
+
+	for _, tt := range []struct {
+		name string
+		val  string
+		ret  flag.VSockRoutes
+		err  error
+	}{
+		{name: "badvsockport", val: "1x010=unix:port", ret: nil, err: strconv.ErrSyntax},
+		{name: "badlocalnet", val: "1x010=unixport", ret: nil, err: strconv.ErrSyntax},
+		{name: "1partno=", val: "1x010unixport", ret: nil, err: strconv.ErrSyntax},
+		{name: "1partno=", val: "1x010unixport", ret: nil, err: strconv.ErrSyntax},
+		{name: "2routesbad2ndrouteno=", val: "17010=tcp:17010,18010unix:port", ret: nil, err: strconv.ErrSyntax},
+		{name: "2routesbad2ndrouteno:", val: "17010=tcp:17010,18010=unixport", ret: nil, err: strconv.ErrSyntax},
+		{name: "2routesbadvsockportin2nd", val: "17010=tcp:17010,1x010=unixport", ret: nil, err: strconv.ErrSyntax},
+
+		{name: "uds", val: "17010=unix:port", ret: flag.VSockRoutes{&flag.VSockRoute{VMPort: 17010, Net: "unix", Addr: "port"}}, err: nil},
+		{name: "tcp", val: "17010=tcp:17010", ret: flag.VSockRoutes{&flag.VSockRoute{VMPort: 17010, Net: "tcp", Addr: "17010"}}, err: nil},
+		{name: "2ports", val: "17010=tcp:17010,18010=unix:port",
+			ret: flag.VSockRoutes{
+				&flag.VSockRoute{VMPort: 17010, Net: "tcp", Addr: "17010"},
+				&flag.VSockRoute{VMPort: 18010, Net: "unix", Addr: "port"},
+			},
+			err: nil},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			r, err := flag.ParseRoutes(strings.Split(tt.val, ",")...)
+			if !errors.Is(err, tt.err) || !reflect.DeepEqual(r, tt.ret) {
+				t.Errorf("ParseRoutes(%s): ! errors.Is(%v, %v) || ! reflect.DeepEqual(%v,%v)", tt.val, err, tt.err, r.String(), tt.ret.String())
+			}
+		})
+	}
+}
 
 func TestParseArg(t *testing.T) {
 	t.Parallel()
@@ -29,6 +67,9 @@ func TestParseArg(t *testing.T) {
 		"1G",
 		"-T",
 		"1M",
+		"-R",
+		"17010=unix:port",
+		"-vsock", "xyz",
 	}
 
 	c, err := flag.ParseArgs(args)
@@ -70,6 +111,14 @@ func TestParseArg(t *testing.T) {
 
 	if c.TraceCount != 1<<20 {
 		t.Errorf("trace: got %#x, want %#x", c.TraceCount, 1<<20)
+	}
+
+	if len(c.Routes) != 1 {
+		t.Errorf("trace: Routes: len(c.Routes) is %d, not 1", len(c.Routes))
+	}
+
+	if c.Vsock != "xyz" {
+		t.Errorf("vsock device: got %q, want %q", c.Vsock, "xyz")
 	}
 }
 
